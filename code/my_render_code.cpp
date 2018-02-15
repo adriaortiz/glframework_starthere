3e63adcb8956b327f98f425a7eed462fd539bf8a
Adrià Ortiz <veric00@gmail.com>
2018-02-15 14:49:45 +0100

    Add project files.

---
+++ code/my_render_code.cpp
@@ -0,0 +1,13 @@
+
+#include <GL\glew.h>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+
+#include <cstdio>
+#include <cassert>
+
+#include "GL_framework.h"
+
+
+//Aqui es on té d' estar TOT el codi de la practica 1 
+
